3ea282591e8846d8c4435c93f6997bff1b23e76a
SandadiLokanathaReddy <sandadiln5102001@gmail.com>
2022-09-26 17:24:17 +0530

    heroku1

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const methodOverride = require('method-override')
+const Article = require('./models/article')
+const article = require("./models/article")
+const articleRouter = require("./routes/articles")
+const app = express()
+
+// for heroku deployment
+const port = process.env.PORT || 5000
+
+
+mongoose.connect("mongodb+srv://lokanath:markdownblog@markdown-blog-cluster.fhazg2g.mongodb.net/test",  {useNewUrlParser : true, useUnifiedTopology : true})
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt : 'desc'})
+    res.render('articles/index', {articles : articles})
+})
+
+app.listen(port, function(){
+    console.log("Server is running at port:",port)
+})
+app.use('/articles', articleRouter)
